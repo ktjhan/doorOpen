03abbb4a7fdd4b017b76c72d25d44466cd39ae84
Kavin Tjhan <tjhan.kavin@gmail.com>
2023-09-30 19:48:42 -0500

    fourth commit

--- frontend/src/components/SearchBar.tsx
+++ frontend/src/components/SearchBar.tsx
@@ -2,24 +2,42 @@ import React, { useState } from "react";
 
 const SearchBar: React.FC = () => {
   const [searchPrompt, setSearchPrompt] = useState("");
-  
+  const [apiResponse, setApiResponse] = useState("");
+
   const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setSearchPrompt(event.target.value);
   };
 
   const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
-    const response = await fetch(`http://localhost:4001/fetch-gpt?prompt=${searchPrompt}`);
-    const results = await response.json();
-    console.log(results);
-    return results;
+    try {
+      const response = await fetch(
+        `http://localhost:4001/fetch-gpt?prompt=${searchPrompt}`
+      );
+      const results = await response.text();
+      setApiResponse(results);
+      console.log(results);
+      return results;
+    } catch (error) {
+      console.error("Fetch error:", error);
+    }
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <input type='text' placeholder='Type a prompt...' value={searchPrompt} onChange={handleChange} />
-      <button type='submit'>Search</button>
-    </form>
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Type a prompt..."
+          value={searchPrompt}
+          onChange={handleChange}
+        />
+        <button type="submit">Search</button>
+      </form>
+      <div>
+        <p className="answer">{apiResponse}</p>
+      </div>
+    </div>
   );
 };
 
